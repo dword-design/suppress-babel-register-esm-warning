2a3edc8dd4bf33bee092547022c390527e2b475a
Sebastian Landwehr <info@sebastianlandwehr.com>
2022-12-03 21:04:51 +0000

    feat: impl

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
-const defaultEmit = process.emit
-process.emit = function (...args) {
-  return defaultEmit.call(this, ...args)
+const defaultEmitWarning = process.emitWarning
+process.emitWarning = function (message, type, ...rest) {
+  if (
+    type === 'DeprecationWarning' &&
+    message ===
+      'Obsolete loader hook(s) supplied and will be ignored: getFormat, transformSource'
+  ) {
+    return undefined
+  }
+
+  return defaultEmitWarning.call(this, message, type, ...rest)
 }
